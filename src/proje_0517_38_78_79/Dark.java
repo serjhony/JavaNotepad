0e0d86327f68de7a3118b212bcfb03836d6a1c1a
serjhony <sercanbayram5@gmail.com>
2022-03-03 13:17:21 +0300

    Karanlık

--- src/proje_0517_38_78_79/Dark.java
+++ src/proje_0517_38_78_79/Dark.java
@@ -1,5 +1,7 @@
 package proje_0517_38_78_79;
 
+import javax.sound.sampled.SourceDataLine;
+
 /**
  *
  * @author {Sercan Bayram:05170000038, Egemen Aydın:05170000078, Oğuz Türk:05170000079} 
@@ -9,6 +11,8 @@ public class Dark implements Theme{
     @Override
     public void tema() {
         System.out.println("Dark Theme Selected.");
-    }
+        System.out.println("Karanlık Tema");
+        System.out.println("Karanlık Tema");
+        System.out.println("Karanlık Tema");
     
 }
